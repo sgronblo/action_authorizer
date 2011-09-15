e13b84976cc628ffe11352882051a03e596d3daa
Sam Grönblom <sgronblo@gmail.com>
2011-09-15 17:06:15 +0300

    Create ActionAuthorizer instance

--- init.rb
+++ init.rb
@@ -3,4 +3,4 @@ require 'action_authorizer'
 
 permissions_config_file = File.expand_path(File.join('config', 'permissions.yml'), Rails.root)
 
-ActionAuthorizer.ACTION_GROUPS = YAML.load_file(permissions_config_file)
+action_authorizer = ActionAuthorizer.new(YAML.load_file(permissions_config_file))
